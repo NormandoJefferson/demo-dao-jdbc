e05080267167ab003d174b33546ba43daea294e5
Normando Jefferson <normandojefferson@hotmail.com>
2021-12-03 18:27:01 -0300

    Reusing instantiation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -14,14 +14,8 @@ import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
 
-    // Vamos criar uma dependência com a conexão, para não
-    // termos que ficar usando conn em todos os métodos.
-    // Vamos ter esse objeto conn a disposição em qualquer lugar da
-    // classe SellerDaoJDBC.
     private Connection conn;
 
-    // Vamos forçar a injeção de dependência, criando um construtor
-    // que recebe um conn como parâmetro.
     public SellerDaoJDBC(Connection conn) {
         this.conn = conn;
     }
@@ -54,43 +48,44 @@ public class SellerDaoJDBC implements SellerDao {
                             + "FROM seller INNER JOIN department "
                             + "ON seller.DepartmentId = department.Id "
                             + "WHERE seller.Id = ?");
-            // A primeira interrogação recebe o 'id' que chegou como parâmetro na função.
+
             st.setInt(1, id);
             rs = st.executeQuery();
-            // O 'rs' traz um objeto no formato de tabela, mas vamos ter que
-            // transformar essa tabela em objeto.
-            // rs.next testa se veio algum resultado, se a consulta não retornou
-            // nenhum registro o rs da falso e pula para o return null, porém se
-            // existir registro significa que retornou uma tabela.
-            // Vamos então transformar essa tabela em objeto e retornar ese objeto.
             if (rs.next()) {
-                // Instanciamos um departamento para depois setar os valores dele.
-                Department dep = new Department();
-                // Setamos o 'id' do objeto dep. Usamos o rs.getInt, pois 'id' é um número e
-                // colocamos o nome da coluna onde ele está.
-                dep.setId(rs.getInt("DepartmentId"));
-                dep.setName(rs.getString("DepName"));
-                // Agora vamos instanciar um vendedor para depois setar os valores dele.
-                Seller obj = new Seller();
-                obj.setId(rs.getInt("Id"));
-                obj.setName(rs.getString("Name"));
-                obj.setEmail(rs.getString("Email"));
-                obj.setBaseSalary(rs.getDouble("BaseSalary"));
-                obj.setBirthDate(rs.getDate("BirthDate"));
-                obj.setDepartment(dep);
+                Department dep = instantiateDepartment(rs);
+                Seller obj = instantiateSeller(rs, dep);
                 return obj;
             }
             return null;
         }
         catch (SQLException e) {
-            // Caso ocorra alguma exceção vamos lançar a nossa exceção
-            // personalizada passando a msg da exceção original.
             throw new DbException(e.getMessage());
         }
         finally {
             DB.closeStatement(st);
             DB.closeResultSet(rs);
         }
+    }
+
+    // Método para instanciar um vendedor através dos dados da tabela.
+    // Vamos propagar a exceção, pois ela já é tratada no método findById.
+    private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+        Seller obj = new Seller();
+        obj.setId(rs.getInt("Id"));
+        obj.setName(rs.getString("Name"));
+        obj.setEmail(rs.getString("Email"));
+        obj.setBaseSalary(rs.getDouble("BaseSalary"));
+        obj.setBirthDate(rs.getDate("BirthDate"));
+        obj.setDepartment(dep);
+        return obj;
+    }
+
+    // Método para instanciar um departamento através dos dados da tabela.
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("DepartmentId"));
+        dep.setName(rs.getString("DepName"));
+        return dep;
     }
 
     @Override
